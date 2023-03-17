ffe7bb8dbdbb5da1d1610ccebc1b9439ab7b6751
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2023-03-17 15:34:29 -0300

    feat(Login and Register):

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,132 @@
+import { ChangeEvent, useContext, useState } from 'react'
+import { Link, NavigateFunction, useNavigate } from 'react-router-dom'
+import { Checkbox } from '../../components/CheckBox'
+
+import {
+  Background,
+  Body,
+  Button,
+  Container,
+  ContainerImput,
+  Header,
+  Imput,
+  LogoAgile,
+  SecurityImput,
+  Text,
+} from './style'
+
+import Logo from '../../assets/logo-agile-branca.png'
+
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
+
+import authService from '../../services/Api/Request/auth'
+import { DataContext } from '../../context/DataContext'
+import { storeLocalData } from '../../services/LocalStorageService'
+import { NavHeader } from '../NavHeader'
+import { Alerta } from '../../components/Alert'
+
+export const Login = () => {
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+  const [hidePass, setHidePass] = useState(true)
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+  const { armazenaDadosUsuarioLogin } = useContext(DataContext)
+
+  const navigate: NavigateFunction = useNavigate()
+
+  const handleLogin = async () => {
+    let tokenJwt: any = null
+
+    try {
+      const retorno = await authService.sigin(login, password)
+
+      if (retorno) {
+        tokenJwt = retorno
+        armazenaDadosUsuarioLogin(tokenJwt)
+        storeLocalData('user', tokenJwt)
+        navigate('/user')
+        Alerta(login, 'Seja Bem vindo!')
+      }
+    } catch (error) {
+      Alerta('Oops!', 'Login ou senha errados')
+    }
+  }
+
+  function handleCheckboxChange(checked: boolean) {
+    setIsChecked(checked)
+  }
+
+  function handleLoginChange(event: ChangeEvent<HTMLInputElement>) {
+    setLogin(event.target.value)
+  }
+  function handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value)
+  }
+  return (
+    <>
+      <Header>
+        <NavHeader />
+      </Header>
+      <Body>
+        <Background>
+          <LogoAgile src={Logo} />
+          <Container>
+            <ContainerImput>
+              <Imput
+                value={login}
+                placeholder="Login"
+                onChange={handleLoginChange}
+              />
+
+              <SecurityImput>
+                <Imput
+                  value={password}
+                  placeholder="Senha"
+                  onChange={handlePasswordChange}
+                  type={hidePass === true ? 'password' : 'text'}
+                />
+
+                {hidePass ? (
+                  <>
+                    <AiFillEye
+                      onClick={() => setHidePass(!hidePass)}
+                      color="#00469e"
+                      size={25}
+                      style={{ marginRight: 20 }}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <AiFillEyeInvisible
+                      onClick={() => setHidePass(!hidePass)}
+                      color="#00469e"
+                      size={25}
+                      style={{ marginRight: 20 }}
+                    />
+                  </>
+                )}
+              </SecurityImput>
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                }}
+              >
+                <Link to={'/register'}>
+                  <Text>Não possuo cadastro</Text>
+                </Link>
+                <Checkbox
+                  label="Lembrar login e senha"
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+            </ContainerImput>
+          </Container>
+        </Background>
+        <Button onClick={() => handleLogin()}>Entrar</Button>
+      </Body>
+    </>
+  )
+}
---
+++ src/pages/Login/style.ts
@@ -0,0 +1,138 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+  height: 80px;
+  background-image: radial-gradient(
+    circle at 120.71% 70%,
+    #c9e2ff 0,
+    #82adff 25%,
+    #3c78f2 50%,
+    #00469e 75%,
+    #001b53 100%
+  );
+`
+export const Body = styled.body`
+  display: flex;
+  width: 100%;
+  margin-top: 100px;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Background = styled.div`
+  width: 500px;
+  height: 500px;
+
+  display: flex;
+  flex-direction: column;
+
+  border-top-right-radius: 20%;
+  border-top-left-radius: 20%;
+  border-bottom-right-radius: 20%;
+  border-bottom-left-radius: 20%;
+
+  background-image: radial-gradient(
+    circle at 120.71% 70%,
+    #c9e2ff 0,
+    #82adff 25%,
+    #3c78f2 50%,
+    #00469e 75%,
+    #001b53 100%
+  );
+
+  @media (max-width: 520px) {
+    width: 350px;
+    height: 450px;
+  }
+  @media (max-width: 520px) {
+    width: 350px;
+    height: 450px;
+  }
+`
+
+export const LogoAgile = styled.img`
+  width: 140px;
+  height: 140px;
+
+  display: flex;
+  align-self: center;
+
+  margin-bottom: 40px;
+`
+
+export const ContainerImput = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  width: 70%;
+  gap: 20px;
+`
+
+export const Imput = styled.input`
+  height: 50px;
+  width: 100%;
+
+  border-radius: 10px;
+  border: 2px solid #fff;
+
+  padding-left: 20px;
+  ::placeholder {
+    color: black;
+  }
+
+  outline: none;
+
+  &:hover {
+    border-color: #fff;
+  }
+`
+
+export const Button = styled.button`
+  width: 300px;
+
+  margin-top: 50px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+
+  border-radius: 20px;
+
+  font-size: 20px;
+  &:hover {
+    color: #fff;
+  }
+  background-image: radial-gradient(
+    circle at 120.71% 70%,
+    #c9e2ff 0,
+    #82adff 25%,
+    #3c78f2 50%,
+    #00469e 75%,
+    #001b53 100%
+  );
+
+  @media (max-width: 520px) {
+    width: 250px;
+  }
+`
+export const SecurityImput = styled.div`
+  background-color: white;
+
+  display: flex;
+  align-items: center;
+  height: 50px;
+  input {
+    width: 100%;
+  }
+
+  border-radius: 10px;
+  border: 2px solid #fff;
+`
+
+export const Text = styled.p`
+  color: white;
+`
